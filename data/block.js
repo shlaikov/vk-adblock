d09fc90c0f0418c4880c30386ff4a681e1c2903e
shlaikov <shlaikov.alexey@ya.ru>
2017-04-12 09:10:36 +0300

    Fixed some issue.

---
+++ data/block.js
@@ -0,0 +1,24 @@
+(function () {
+    'use strict'
+
+
+    var docker = document.getElementById("side_bar_inner");
+
+    if (!docker) {
+        return;
+    }
+
+    var target = [
+        "ads_left",
+        "left_ads"
+    ];
+
+    for (var i = 0, len = target.length; i < len; i++) {
+
+        var ads = document.getElementById(target[i]);
+        if (ads) {
+            docker.removeChild(ads);
+        }
+    }
+
+})();
